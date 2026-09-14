10b7d667a2d109a5bdf65b5e0d5efef0e2825aaf
lffzdsf <luizfelipedsf@gmail.com>
2026-09-14 17:46:59 -0300

    Conecta portal ao Web App do Google Apps Script

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 // URL pública do Web App do Google Apps Script.
-// Preenchida após o deploy do backend vinculado à planilha do Clube.
-window.CLUBE_API_URL = "";
+window.CLUBE_API_URL = "https://script.google.com/macros/s/AKfycbzwfWrPmYoalKH_mn-mRAClckFHY-sNITWYQvZM5QdSFddj4Flkn4pAy7Q2AsLIhG7UyQ/exec";
